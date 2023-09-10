8be17e386b9d296dbc04179faa6cef48fb1e272f
Ryan Sun <ryansun222@gmail.com>
2023-09-09 17:21:33 -0700

    added deps.ts file to centralize imports

---
+++ src/deps.ts
@@ -0,0 +1,10 @@
+// centralized location to import and re-export external libraries
+
+// ex:
+//  export {
+//    assert,
+//    assertEquals,
+//    assertStringIncludes,
+//  } from "https://deno.land/std@0.201.0/assert/mod.ts";
+// import like this
+//  import { assertEquals, runTests, test } from "./deps.ts";
